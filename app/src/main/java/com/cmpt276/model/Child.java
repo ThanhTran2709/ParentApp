63c70d4c8315f922dd1ad748336dd71d5912f10b
Divya Soneji <divya.soneji@hotmail.com>
2021-10-26 09:24:29 -0700

    Added a constructor for Child class and added a variable, getter and setter for age

--- app/src/main/java/com/cmpt276/model/Child.java
+++ app/src/main/java/com/cmpt276/model/Child.java
@@ -2,12 +2,26 @@ package com.cmpt276.model;
 
 public class Child {
     private String name;
+    private int age;
+
+    public Child(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public void setAge(int age){
+        this.age = age;
+    }
+
     public String getName() {
-        return name;
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
     }
 }
