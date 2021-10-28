4d5a9938cc1d592de3ad97672f7c623dcfbbfd34
Divya Soneji <divya.soneji@hotmail.com>
2021-10-28 14:44:27 -0700

    Added added TextView and Buttons in content_main.xml

--- app/src/main/java/com/cmpt276/parentapp/MainActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/MainActivity.java
@@ -4,10 +4,29 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.cmpt276.parentapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setUpMyChildrenButton();
+        setUpCoinFlipButton();
+        setUpTimerButton();
+    }
+
+    private void setUpTimerButton() {
+    }
+
+    private void setUpCoinFlipButton() {
+    }
+
+    private void setUpMyChildrenButton() {
     }
 }
