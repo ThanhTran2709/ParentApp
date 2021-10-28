e868cc66829ef370d3aa8e51bb56e89ead3271b7
Divya Soneji <divya.soneji@hotmail.com>
2021-10-28 15:05:07 -0700

    Set up intents for the My Children, Flip A Coin, and Timer buttons in MainActivity

--- app/src/main/java/com/cmpt276/parentapp/MainActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.cmpt276.parentapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,11 +25,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUpTimerButton() {
+        Button timerButton = findViewById(R.id.timer_button);
+        timerButton.setOnClickListener(view -> {
+            Intent i = TimerActivity.getIntent(MainActivity.this);
+            startActivity(i);
+        });
     }
 
     private void setUpCoinFlipButton() {
+        Button coinFlipButton = findViewById(R.id.flip_a_coin_button);
+        coinFlipButton.setOnClickListener(view -> {
+            Intent i = CoinFlipActivity.getIntent(MainActivity.this);
+            startActivity(i);
+        });
     }
 
     private void setUpMyChildrenButton() {
+        Button myChildrenButton = findViewById(R.id.my_children_button);
+        myChildrenButton.setOnClickListener(view -> {
+            Intent i = ChildrenActivity.getIntent(MainActivity.this);
+            startActivity(i);
+        });
     }
 }
---
+++ app/src/main/java/com/cmpt276/parentapp/TimerActivity.java
@@ -0,0 +1,20 @@
+package com.cmpt276.parentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class TimerActivity extends AppCompatActivity {
+
+    public static Intent getIntent(Context context){
+        return new Intent(context, TimerActivity.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
