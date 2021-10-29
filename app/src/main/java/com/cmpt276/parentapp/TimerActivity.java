a181b3d13886830d5fecee22fa1b20f48561a307
Divya Soneji <divya.soneji@hotmail.com>
2021-10-28 18:09:19 -0700

    Created Pause/Play, Reset, and Timer Options buttons in activity_timer.xml, and created an intent for Timer Options button in TimerActivity

--- app/src/main/java/com/cmpt276/parentapp/TimerActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/TimerActivity.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class TimerActivity extends AppCompatActivity {
+
+    private CountDownTimer timer;
 
     public static Intent getIntent(Context context){
         return new Intent(context, TimerActivity.class);
@@ -16,5 +23,27 @@ public class TimerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+        setUpTimerOptionsButton();
+        setUpPausePlayButton();
+        setUpResetButton();
+        setupTimeOutTimer();
+
+    }
+
+    private void setupTimeOutTimer() {
+    }
+
+    private void setUpResetButton() {
+    }
+
+    private void setUpPausePlayButton() {
+    }
+
+    private void setUpTimerOptionsButton() {
+        Button timerOptions = findViewById(R.id.timer_options_button);
+        timerOptions.setOnClickListener(view -> {
+            Intent i = TimerOptions.getIntent(TimerActivity.this);
+            startActivity(i);
+        });
     }
 }
