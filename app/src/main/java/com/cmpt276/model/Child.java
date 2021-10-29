0a809dabeff9cdbd7ee0365c5ba00acaaf02bd0d
Divya Soneji <divya.soneji@hotmail.com>
2021-10-29 13:37:47 -0700

    Removed the age variable from Child model

--- app/src/main/java/com/cmpt276/model/Child.java
+++ app/src/main/java/com/cmpt276/model/Child.java
@@ -2,26 +2,17 @@ package com.cmpt276.model;
 
 public class Child {
     private String name;
-    private int age;
 
     public Child(String name, int age){
         this.name = name;
-        this.age = age;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setAge(int age){
-        this.age = age;
-    }
-
     public String getName() {
         return this.name;
     }
 
-    public int getAge(){
-        return this.age;
-    }
 }
