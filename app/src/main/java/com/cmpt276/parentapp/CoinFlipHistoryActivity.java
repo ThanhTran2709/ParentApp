b7bd23a0b7312f08e17e79167b93aad9eda254a6
Jocelyn Tandrea <jocelyntandrea@gmail.com>
2021-11-06 13:29:57 -0700

    Add custom listview for flip history

--- app/src/main/java/com/cmpt276/parentapp/CoinFlipHistoryActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/CoinFlipHistoryActivity.java
@@ -2,17 +2,26 @@ package com.cmpt276.parentapp;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.cmpt276.model.Child;
 import com.cmpt276.model.Coin;
 import com.cmpt276.model.Options;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -40,22 +49,44 @@ public class CoinFlipHistoryActivity extends AppCompatActivity {
 
 	private View.OnClickListener getClearListener() {
 		return view -> {
-			AlertDialog.Builder builder = new AlertDialog.Builder(CoinFlipHistoryActivity.this);
-
-			builder.setTitle(R.string.confirm_reset);
-
-			builder.setPositiveButton(R.string.reset, (dialogInterface, i) -> {
-				options.clearCoinFlips(CoinFlipHistoryActivity.this);
-				CoinFlipHistoryActivity.this.finish();
-			});
-
-			builder.setNegativeButton(R.string.cancel, (dialogInterface, i) -> {
-				dialogInterface.dismiss();
-			});
-
-			AlertDialog dialog = builder.create();
-			dialog.show();
+			if(options.getFlipHistory(this).size() == 0) {
+				Toast.makeText(this, "There are no coin flip history to clear", Toast.LENGTH_SHORT).show();
+			}
+			else {
+				ClearHistoryDialog alert = new ClearHistoryDialog();
+				alert.showDialog(CoinFlipHistoryActivity.this);
+			}
 		};
+	}
+
+	public class ClearHistoryDialog {
+
+		public void showDialog(Activity activity) {
+			final Dialog dialog = new Dialog(activity);
+			dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+			dialog.setCancelable(false);
+			dialog.setContentView(R.layout.clear_dialog);
+			dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+			FloatingActionButton cancelFab = dialog.findViewById(R.id.cancelFab);
+			cancelFab.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					dialog.dismiss();
+				}
+			});
+
+			FloatingActionButton addFab = dialog.findViewById(R.id.okFab);
+			addFab.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					options.clearCoinFlips(CoinFlipHistoryActivity.this);
+					CoinFlipHistoryActivity.this.finish();
+				}
+			});
+
+			dialog.show();
+		}
 	}
 
 	public static Intent getIntent(Context context){
