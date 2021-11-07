6d3f3608dd065eb7689c7a1117b6cecaa631b065
Divya Soneji <divya.soneji@hotmail.com>
2021-11-07 10:27:26 -0800

    TimerService receives broadcasts from TimerActivity for pausing and playing and resetting (cancelling and recreating the CountDownTimer)

--- app/src/main/java/com/cmpt276/parentapp/TimerService.java
+++ app/src/main/java/com/cmpt276/parentapp/TimerService.java
@@ -24,12 +24,20 @@ public class TimerService extends Service {
     private static final String TIMER_SERVICE_BROADCAST = "timer_service_broadcast";
     private static final String MAIN_ACTIVITY_BROADCAST = "main_activity_broadcast";
     private static final String START_SERVICE_BROADCAST = "start_service_broadcast";
+    private static final String RESET_TIMER_BROADCAST = "reset_timer_broadcast";
     public static final String CHANNEL_ID = "timer_service_channel";
+    private static final String PAUSE_TIMER_BROADCAST = "pause_timer_broadcast";
+    private static final String PLAY_TIMER_BROADCAST = "play_timer_broadcast";
+    private static final String ACTION_STOP_ALARM = "action_stop_alarm";
+    private static final String STOP_ALARM_ID = "stop_alarm_id";
 
     private CountDownTimer timer;
     private long remainingMilliSeconds;
     private long originalTimeInMilliSeconds;
     private BroadcastReceiver mainActivityReceiver;
+    private BroadcastReceiver pauseTimerReceiver;
+    private BroadcastReceiver playTimerReceiver;
+    private BroadcastReceiver resetTimerReceiver;
 
     public static Intent getIntent(Context context, long remainingMilliSeconds, long originalTimeInMilliSeconds){
         Intent i = new Intent(context, TimerService.class);
@@ -45,8 +53,51 @@ public class TimerService extends Service {
         remainingMilliSeconds = intent.getLongExtra(MINUTES_IN_MILLI_SECONDS_TAG, DEFAULT_MINUTES_IN_MILLI_SECONDS);
         originalTimeInMilliSeconds = intent.getLongExtra(ORIGINAL_TIME_IN_MILLI_SECONDS_TAG, DEFAULT_MINUTES_IN_MILLI_SECONDS);
         communicateWithMainActivity();
-        setUpTimerBroadcast();
+        setUpTimerBroadcast(remainingMilliSeconds);
+        setUpPauseBroadcastReceiver();
+        setUpPlayBroadcastReceiver();
+        setUpResetBroadcastReceiver();
         return START_STICKY;
+    }
+
+    private void setUpResetBroadcastReceiver() {
+        resetTimerReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                timer.cancel();
+                long milliSeconds = intent.getLongExtra(MINUTES_IN_MILLI_SECONDS_TAG, DEFAULT_MINUTES_IN_MILLI_SECONDS);
+                setUpTimerBroadcast(milliSeconds);
+            }
+        };
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(RESET_TIMER_BROADCAST);
+        registerReceiver(resetTimerReceiver, filter);
+    }
+
+    private void setUpPauseBroadcastReceiver() {
+        pauseTimerReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                timer.cancel();
+            }
+        };
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(PAUSE_TIMER_BROADCAST);
+        registerReceiver(pauseTimerReceiver, filter);
+
+    }
+
+    private void setUpPlayBroadcastReceiver() {
+        playTimerReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                long milliSeconds = intent.getLongExtra(MINUTES_IN_MILLI_SECONDS_TAG, DEFAULT_MINUTES_IN_MILLI_SECONDS);
+                setUpTimerBroadcast(milliSeconds);
+            }
+        };
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(PLAY_TIMER_BROADCAST);
+        registerReceiver(playTimerReceiver, filter);
     }
 
     private void communicateWithMainActivity() {
@@ -67,12 +118,16 @@ public class TimerService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        unregisterReceiver(mainActivityReceiver);
+        unregisterReceiver(resetTimerReceiver);
+        unregisterReceiver(playTimerReceiver);
+        unregisterReceiver(pauseTimerReceiver);
         timer.cancel();
     }
 
-    private void setUpTimerBroadcast() {
+    private void setUpTimerBroadcast(long milliSeconds) {
 
-        timer = new CountDownTimer(remainingMilliSeconds, 1000) {
+        timer = new CountDownTimer(milliSeconds, 1000) {
 
             @Override
             public void onTick(long l) {
@@ -86,26 +141,34 @@ public class TimerService extends Service {
             @Override
             public void onFinish() {
                 setUpNotification();
+
             }
         };
         timer.start();
     }
 
     public void setUpNotification(){
-        Intent notificationIntent = MainActivity.getIntent(this);
-        final PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-        final Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+        Intent stopAlarmIntent = TimerActivity.getIntent(this, originalTimeInMilliSeconds, true);
+        stopAlarmIntent.setAction(ACTION_STOP_ALARM);
+        stopAlarmIntent.putExtra(STOP_ALARM_ID, 0);
+        PendingIntent stopAlarmPendingIntent =
+                PendingIntent.getBroadcast(this, 0, stopAlarmIntent, PendingIntent.FLAG_IMMUTABLE);
+        Intent notificationIntent = TimerActivity.getIntent(this, remainingMilliSeconds, true);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
         .setContentTitle("Timeout Timer")
         .setContentText("Time's Up!")
         .setSmallIcon(R.drawable.ic_baseline_check_circle_24)
         .setContentIntent(pendingIntent)
         .setAutoCancel(true)
+                .addAction(R.drawable.ic_launcher_background, "STOP", stopAlarmPendingIntent)
                 .build();
 
-        startForeground(1, notification);
         NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "Timer Service", NotificationManager.IMPORTANCE_DEFAULT);
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
         notificationManager.createNotificationChannel(notificationChannel);
+        startForeground(1, notification);
 
     }
 
