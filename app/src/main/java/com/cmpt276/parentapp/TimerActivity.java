90f9788334b898b9838be3f79acba4e2d1c8b999
Divya Soneji <divya.soneji@hotmail.com>
2021-11-28 20:22:53 -0800

    Created toolbar that includes a speed selection button. Set up a speed selection dialog for that button. Added a TextView that displays the selected speed.

--- app/src/main/java/com/cmpt276/parentapp/TimerActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/TimerActivity.java
@@ -1,7 +1,10 @@
 package com.cmpt276.parentapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -9,289 +12,337 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.Toolbar;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Contains the UI for the timer
  */
 public class TimerActivity extends AppCompatActivity {
 
-	private static final String ORIGINAL_TIME_IN_MILLI_SECONDS_TAG = "original_time_in_milli_seconds_tag";
-	private static final String TIMER_SERVICE_BROADCAST = "timer_service_broadcast";
-	private static final String STOP_ALARM_BROADCAST = "stop_alarm_broadcast";
-	private static final String SERVICE_RUNNING_FLAG = "service_running_flag";
-	private static final long DEFAULT_MINUTES_IN_MILLI_SECONDS = 0L;
-
-	TextView remainingTime;
-	private Intent serviceIntent;
-	private long originalTimeInMilliSeconds;
-	private boolean isServiceRunning;
-
-	BroadcastReceiver timerReceiver;
-	BroadcastReceiver stopAlarmReceiver;
-
-	TimerService timerService;
-	private boolean timerServiceBound = false;
-
-
-	public static Intent getIntent(Context context, long minutesInMilliSeconds) {
-		return TimerActivity.getIntent(context, minutesInMilliSeconds, false);
-	}
-
-	public static Intent getIntent(Context context,
-								   long originalTimeInMilliSeconds,
-								   boolean isServiceRunning) {
-
-		Intent intent = new Intent(context, TimerActivity.class);
-		intent.putExtra(ORIGINAL_TIME_IN_MILLI_SECONDS_TAG, originalTimeInMilliSeconds);
-		intent.putExtra(SERVICE_RUNNING_FLAG, isServiceRunning);
-
-		return intent;
-
-	}
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_timer);
-
-		originalTimeInMilliSeconds = this.getIntent().getLongExtra(ORIGINAL_TIME_IN_MILLI_SECONDS_TAG, DEFAULT_MINUTES_IN_MILLI_SECONDS);
-		isServiceRunning = this.getIntent().getBooleanExtra(SERVICE_RUNNING_FLAG, false);
-		setUpToolBar();
-		setUpPieChart();
-		setUpPausePlayButton();
-		setUpResetButton();
-		setUpNewTimerButton();
-		//setUpBackBtn();
+    private static final String ORIGINAL_TIME_IN_MILLI_SECONDS_TAG = "original_time_in_milli_seconds_tag";
+    private static final String TIMER_SERVICE_BROADCAST = "timer_service_broadcast";
+    private static final String STOP_ALARM_BROADCAST = "stop_alarm_broadcast";
+    private static final String SERVICE_RUNNING_FLAG = "service_running_flag";
+    private static final long DEFAULT_MINUTES_IN_MILLI_SECONDS = 0L;
+    private static final int DEFAULT_SPEED = 100;
+
+    TextView remainingTime;
+    private Intent serviceIntent;
+    private long originalTimeInMilliSeconds;
+    private boolean isServiceRunning;
+    int speed = DEFAULT_SPEED;
+
+    BroadcastReceiver timerReceiver;
+    BroadcastReceiver stopAlarmReceiver;
+
+    TimerService timerService;
+    private boolean timerServiceBound = false;
 
-	}
 
-	private void setUpToolBar() {
-		setSupportActionBar(findViewById(R.id.timer_toolbar));
+    public static Intent getIntent(Context context, long minutesInMilliSeconds) {
+        return TimerActivity.getIntent(context, minutesInMilliSeconds, false);
+    }
+
+    public static Intent getIntent(Context context,
+                                   long originalTimeInMilliSeconds,
+                                   boolean isServiceRunning) {
+
+        Intent intent = new Intent(context, TimerActivity.class);
+        intent.putExtra(ORIGINAL_TIME_IN_MILLI_SECONDS_TAG, originalTimeInMilliSeconds);
+        intent.putExtra(SERVICE_RUNNING_FLAG, isServiceRunning);
+
+        return intent;
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+
+        originalTimeInMilliSeconds = this.getIntent().getLongExtra(ORIGINAL_TIME_IN_MILLI_SECONDS_TAG, DEFAULT_MINUTES_IN_MILLI_SECONDS);
+        isServiceRunning = this.getIntent().getBooleanExtra(SERVICE_RUNNING_FLAG, false);
+        setUpToolBar();
+        setUpPieChart();
+        setUpPausePlayButton();
+        setUpResetButton();
+        setUpNewTimerButton();
+        setUpSpeedText();
+
+    }
+
+    private void setUpSpeedText() {
+        TextView speedText = findViewById(R.id.speed_text);
+        speedText.setText(getString(R.string.speed_text, speed));
+    }
+
+    private void setUpToolBar() {
+        setSupportActionBar(findViewById(R.id.timer_toolbar));
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    private void setUpPieChart() {
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setMax((int) (originalTimeInMilliSeconds / 1000.0));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.timer_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_speed:
+                displaySpeedSelectionDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-		ActionBar actionBar = getSupportActionBar();
-		actionBar.setDisplayHomeAsUpEnabled(true);
+    private void displaySpeedSelectionDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-	}
+        int[] speeds = getResources().getIntArray(R.array.timer_speeds_array);
+        String[] speed_options = getResources().getStringArray(R.array.speed_string_options_array);
+
+        AtomicInteger selected = new AtomicInteger();
 
-	private void setUpPieChart() {
-		ProgressBar progressBar = findViewById(R.id.progressBar);
-		progressBar.setMax((int)(originalTimeInMilliSeconds /1000.0));
-	}
+        for(int i = 0; i < speeds.length; i++){
+            if(speeds[i] == speed){
+                selected.set(i);
+            }
+        }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.timer_menu, menu);
-		return true;
-	}
+        builder.setTitle(R.string.change_speed);
+        builder.setSingleChoiceItems(speed_options,
+                selected.get(), (dialogInterface, i) -> selected.set(i));
+        builder.setPositiveButton("SELECT", (dialogInterface, i) -> {
+            speed = speeds[selected.get()];
+            setUpSpeedText();
+            dialogInterface.dismiss();
+        });
 
-	//	private void setUpBackBtn() {
-//		Button backBtn = findViewById(R.id.backBtn_timer);
-//		backBtn.setOnClickListener(view -> finish());
-//	}
+        builder.setNeutralButton("CANCEL", (dialogInterface, i) -> dialogInterface.dismiss());
+        builder.show();
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		setUpStartService();
-		setupTimerBroadCastReceiver();
-		setUpStopAlarmReceiver();
-	}
+    }
 
-	@Override
-	protected void onStop() {
-		super.onStop();
-		unregisterReceiver(timerReceiver);
-		unregisterReceiver(stopAlarmReceiver);
-		unbindService(connection);
-		finish();
-	}
 
-	private void setUpStartService() {
-		serviceIntent = TimerService.getIntent(this, originalTimeInMilliSeconds);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setUpStartService();
+        setupTimerBroadCastReceiver();
+        setUpStopAlarmReceiver();
+    }
 
-		if (!isServiceRunning) {
-			startService(serviceIntent);
-			isServiceRunning = true;
-		}
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(timerReceiver);
+        unregisterReceiver(stopAlarmReceiver);
+        unbindService(connection);
+        finish();
+    }
 
-		bindService(serviceIntent, connection, 0);
-	}
+    private void setUpStartService() {
+        serviceIntent = TimerService.getIntent(this, originalTimeInMilliSeconds);
 
-	private void setupTimerBroadCastReceiver() {
-		remainingTime = findViewById(R.id.time_text);
+        if (!isServiceRunning) {
+            startService(serviceIntent);
+            isServiceRunning = true;
+        }
 
-		timerReceiver = new BroadcastReceiver() {
-			@Override
-			public void onReceive(Context context, Intent intent) {
-				updateTimerLabelAndChart();
-			}
-		};
+        bindService(serviceIntent, connection, 0);
+    }
 
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(TIMER_SERVICE_BROADCAST);
-		registerReceiver(timerReceiver, filter);
-	}
+    private void setupTimerBroadCastReceiver() {
+        remainingTime = findViewById(R.id.time_text);
 
-	private void updateTimerLabelAndChart() {
-		TextView timeText = findViewById(R.id.time_text);
-		timeText.setText(timerService.getTimeString());
+        timerReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                updateTimerLabelAndChart();
+            }
+        };
 
-		ProgressBar progressBar = findViewById(R.id.progressBar);
-		progressBar.setProgress((int)timerService.getProgress());
-		Log.i("progress" ,progressBar.getProgress() + " ");
-	}
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(TIMER_SERVICE_BROADCAST);
+        registerReceiver(timerReceiver, filter);
+    }
 
-	private void resetTimer() {
+    private void updateTimerLabelAndChart() {
+        TextView timeText = findViewById(R.id.time_text);
+        timeText.setText(timerService.getTimeString());
 
-		String originalTime = timerService.getOriginalTimeString();
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setProgress((int) timerService.getProgress());
+        Log.i("progress", progressBar.getProgress() + " ");
+    }
 
-		stopService(serviceIntent);
-		isServiceRunning = false;
+    private void resetTimer() {
 
-		TextView timeText = findViewById(R.id.time_text);
-		timeText.setText(originalTime);
+        String originalTime = timerService.getOriginalTimeString();
 
-		ProgressBar progressBar = findViewById(R.id.progressBar);
-		progressBar.setProgress((int)(originalTimeInMilliSeconds / 1000.0));
-	}
+        stopService(serviceIntent);
+        isServiceRunning = false;
 
-	private void setUpNewTimerButton() {
+        TextView timeText = findViewById(R.id.time_text);
+        timeText.setText(originalTime);
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setProgress((int) (originalTimeInMilliSeconds / 1000.0));
+    }
+
+    private void setUpNewTimerButton() {
+
+        Button editTimeButton = findViewById(R.id.new_timer_button);
+
+        editTimeButton.setOnClickListener(view -> {
+
+            if (isServiceRunning) {
+                stopService(serviceIntent);
+                isServiceRunning = false;
+            }
+
+            Intent intent = TimerOptions.getIntent(this);
+            startActivity(intent);
+
+            finish();
+
+        });
+    }
+
+    private void setUpResetButton() {
 
-		Button editTimeButton = findViewById(R.id.new_timer_button);
-
-		editTimeButton.setOnClickListener(view -> {
-
-			if (isServiceRunning) {
-				stopService(serviceIntent);
-				isServiceRunning = false;
-			}
-
-			Intent intent = TimerOptions.getIntent(this);
-			startActivity(intent);
-
-			finish();
-
-		});
-	}
-
-	private void setUpResetButton() {
-
-		Button resetButton = findViewById(R.id.reset_button);
-
-		resetButton.setOnClickListener(view -> resetTimer());
-	}
-
-	private void updatePausePlayButtonText() {
-		Button pausePlayButton = findViewById(R.id.pause_play);
-
-		if ((!timerServiceBound) || timerService.isPaused()) {
-			pausePlayButton.setText(R.string.play_button_text);
-
-		}
-		else {
-			pausePlayButton.setText(R.string.pause_button_text);
-		}
-
-	}
-
-	private void setUpPausePlayButton() {
-		Button pausePlayButton = findViewById(R.id.pause_play);
-
-		pausePlayButton.setOnClickListener(view -> {
-			if (!timerServiceBound) {
-				setUpStartService();
-			}
-			else {
-				if (timerService.isPaused()) {
-					timerService.playTimer();
-				}
-				else {
-					timerService.pauseTimer();
-					updateTimerLabelAndChart();
-				}
-			}
-			updatePausePlayButtonText();
-		});
-
-	}
-
-	private void setUpStopAlarmReceiver() {
-		stopAlarmReceiver = new BroadcastReceiver() {
-			@Override
-			public void onReceive(Context context, Intent intent) {
-				setUpStopAlarmButton();
-			}
-		};
-
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(STOP_ALARM_BROADCAST);
-		registerReceiver(stopAlarmReceiver, filter);
-	}
-
-	private void setUpStopAlarmButton() {
-
-		if (timerService.isFinish()) {
-			ProgressBar progressBar = findViewById(R.id.progressBar);
-			progressBar.setProgress(0);
-
-			Button stopAlarmButton = findViewById(R.id.stop_alarm_button);
-			Button pausePlayButton = findViewById(R.id.pause_play);
-			Button resetButton = findViewById(R.id.reset_button);
-			Button newTimerButton = findViewById(R.id.new_timer_button);
-
-
-			stopAlarmButton.setVisibility(View.VISIBLE);
-			pausePlayButton.setVisibility(View.INVISIBLE);
-			resetButton.setVisibility(View.INVISIBLE);
-			newTimerButton.setVisibility(View.INVISIBLE);
-
-			stopAlarmButton.setOnClickListener(view -> {
-				timerService.stopSoundAndVibration();
-				resetTimer();
-				updatePausePlayButtonText();
-				stopAlarmButton.setVisibility(View.INVISIBLE);
-				pausePlayButton.setVisibility(View.VISIBLE);
-				resetButton.setVisibility(View.VISIBLE);
-				newTimerButton.setVisibility(View.VISIBLE);
-			});
-		}
-
-	}
-
-	/**
-	 * https://developer.android.com/guide/components/bound-services#Binder
-	 */
-	private ServiceConnection connection = new ServiceConnection() {
-
-		@Override
-		public void onServiceConnected(ComponentName className,
-									   IBinder service) {
-
-			TimerService.LocalBinder binder = (TimerService.LocalBinder) service;
-			timerService = binder.getService();
-			timerServiceBound = true;
-
-			updateTimerLabelAndChart();
-			updatePausePlayButtonText();
-			setUpStopAlarmButton();
-
-		}
-
-		@Override
-		public void onServiceDisconnected(ComponentName arg0) {
-			timerServiceBound = false;
-			updatePausePlayButtonText();
-		}
-	};
+        Button resetButton = findViewById(R.id.reset_button);
+
+        resetButton.setOnClickListener(view -> resetTimer());
+    }
+
+    private void updatePausePlayButtonText() {
+        Button pausePlayButton = findViewById(R.id.pause_play);
+
+        if ((!timerServiceBound) || timerService.isPaused()) {
+            pausePlayButton.setText(R.string.play_button_text);
+
+        } else {
+            pausePlayButton.setText(R.string.pause_button_text);
+        }
+
+    }
+
+    private void setUpPausePlayButton() {
+        Button pausePlayButton = findViewById(R.id.pause_play);
+
+        pausePlayButton.setOnClickListener(view -> {
+            if (!timerServiceBound) {
+                setUpStartService();
+            } else {
+                if (timerService.isPaused()) {
+                    timerService.playTimer();
+                } else {
+                    timerService.pauseTimer();
+                    updateTimerLabelAndChart();
+                }
+            }
+            updatePausePlayButtonText();
+        });
+
+    }
+
+    private void setUpStopAlarmReceiver() {
+        stopAlarmReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                setUpStopAlarmButton();
+            }
+        };
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(STOP_ALARM_BROADCAST);
+        registerReceiver(stopAlarmReceiver, filter);
+    }
+
+    private void setUpStopAlarmButton() {
+
+        if (timerService.isFinish()) {
+            ProgressBar progressBar = findViewById(R.id.progressBar);
+            progressBar.setProgress(0);
+
+            Button stopAlarmButton = findViewById(R.id.stop_alarm_button);
+            Button pausePlayButton = findViewById(R.id.pause_play);
+            Button resetButton = findViewById(R.id.reset_button);
+            Button newTimerButton = findViewById(R.id.new_timer_button);
+
+
+            stopAlarmButton.setVisibility(View.VISIBLE);
+            pausePlayButton.setVisibility(View.INVISIBLE);
+            resetButton.setVisibility(View.INVISIBLE);
+            newTimerButton.setVisibility(View.INVISIBLE);
+
+            stopAlarmButton.setOnClickListener(view -> {
+                timerService.stopSoundAndVibration();
+                resetTimer();
+                updatePausePlayButtonText();
+                stopAlarmButton.setVisibility(View.INVISIBLE);
+                pausePlayButton.setVisibility(View.VISIBLE);
+                resetButton.setVisibility(View.VISIBLE);
+                newTimerButton.setVisibility(View.VISIBLE);
+            });
+        }
+
+    }
+
+    /**
+     * https://developer.android.com/guide/components/bound-services#Binder
+     */
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+
+            TimerService.LocalBinder binder = (TimerService.LocalBinder) service;
+            timerService = binder.getService();
+            timerServiceBound = true;
+
+            updateTimerLabelAndChart();
+            updatePausePlayButtonText();
+            setUpStopAlarmButton();
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            timerServiceBound = false;
+            updatePausePlayButtonText();
+        }
+    };
 
 }
